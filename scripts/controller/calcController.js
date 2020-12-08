67a7071b916cd3fed0263b426e95fcd8c2a49e74
junioralves00 <junioralves00@gmail.com>
2020-12-08 08:15:33 -0300

    Aula C16

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -16,6 +16,7 @@ class CalcController {
         this.initialize();
 
         this.initButtonsEvent();
+        this.initKeyboard();
     }
     
     initialize(){
@@ -28,6 +29,55 @@ class CalcController {
 
         this.setLastNumberToDisplay();
 
+    }
+
+    initKeyboard(){
+
+        document.addEventListener('keyup', e=>{
+
+            switch (e.key) {
+
+                case 'Escape':
+                    this.clearAll();
+                    break;
+    
+                case 'Backspace':
+                    this.clearEntry();
+                    break;
+                
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '%':
+                    this.addOperation(e.key);
+                    break;    
+    
+                case 'Enter':
+                case '=':
+                    this.calc();
+                    break;
+    
+                case '.':
+                case ',':    
+                    this.addDot();
+                    break;
+    
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    this.addOperation(parseInt(e.key));
+                    break;
+            }
+
+        });
     }
 
     addEventListenerAll(element, events, fn,){
@@ -172,7 +222,7 @@ class CalcController {
 
             }else{
                 let newValue = this.getLastOperation().toString() + value.toString();
-                this.setLastOperation(parseFloat(newValue));
+                this.setLastOperation(newValue);
 
                 this.setLastNumberToDisplay();
             }           
@@ -187,6 +237,8 @@ class CalcController {
     addDot (){
 
         let lastOperation = this.getLastOperation();
+
+        if(typeof lastOperation === 'string' && lastOperation.split('').indexOf('.') > -1 ) return;
 
         if(this.isOperator(lastOperation) || !lastOperation){
             this.pushOperation('0.');
